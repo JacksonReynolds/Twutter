f23977684ae0c8c452c7bf40f1329132d0b34586
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-10-31 17:14:35 -0500

    Adds basic styling, adds delete functionality

--- twutter-backend/app/controllers/posts_controller.rb
+++ twutter-backend/app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     render json: post
   end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = 1
+    post.save
+    # byebug
+    render json: post
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- twutter-frontend/src/index.js
+++ twutter-frontend/src/index.js
@@ -1,32 +1,63 @@
-const BASE_URL = "http://localhost:3000";
-const USERS_URL = `${BASE_URL}/users`;
-const POSTS_URL = `${BASE_URL}/posts`;
-const body = document.querySelector("body");
+const posts = document.querySelector(".post-container");
+const post = document.querySelector(".post");
 
-// document.addEventListener("DOMContentLoaded", () => loadPosts());
-
-const loadPosts = () => {
-  fetch(POSTS_URL)
+function fetchPosts() {
+  fetch("http://localhost:3000/posts")
     .then((res) => res.json())
-    .then((json) => {
-      json.forEach((post) => renderPost(post));
-    });
-};
+    .then((posts) => renderPosts(posts));
+}
 
-const renderPost = (postHash) => {
-  const div = document.createElement("div");
-  const userName = document.createElement("a");
+function renderPost(post) {
+  const postContainer = document.createElement("div");
+  postContainer.id = post.id;
+  postContainer.className = "post";
+
+  const userName = document.createElement("h3");
+  userName.innerHTML = post.user.name;
+
   const content = document.createElement("p");
-  const deleteButton = document.createElement("button");
-  const user = (div.className = "post-container");
-  // userName.innerHTML = postHash.user.name;
-  content.innerText = postHash.content;
-  deleteButton.innerText = "Delete";
-  // deleteButton.addEventListener("click", deletePost());
-  div.appendChild(content);
-  div.appendChild(deleteButton);
-  div.appendChild(userName);
-  body.appendChild(div);
-};
+  content.innerText = post.content;
 
-loadPosts();
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
+  deleteButton.addEventListener("click", () => deletePost(post));
+
+  // const timeStamp = document.createElement("div");
+  // timeStamp.innerText = post.created_at;
+
+  postContainer.append(userName, content, deleteButton);
+  posts.appendChild(postContainer);
+}
+
+function renderPosts(posts) {
+  for (let userPost of posts) {
+    renderPost(userPost);
+  }
+}
+
+post.addEventListener("submit", createPost);
+
+function createPost(e) {
+  e.preventDefault();
+  const content = document.querySelector(".twit").value;
+
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ content: content }),
+  })
+    .then((res) => res.json())
+    .then((post) => renderPost(post));
+}
+
+function deletePost(post) {
+  fetch(`http://localhost:3000/posts/${post.id}`, {
+    method: "DELETE",
+  }).then((obj = document.getElementById(post.id)), obj.remove());
+}
+
+fetchPosts();
