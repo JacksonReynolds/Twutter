e897df7cb0c8c384705c5244a9e2cf85296b801b
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-10-31 20:03:03 -0500

    Converted code to OO

--- twutter-frontend/src/index.js
+++ twutter-frontend/src/index.js
@@ -1,63 +1,6 @@
 const posts = document.querySelector(".post-container");
 const post = document.querySelector(".post");
 
-function fetchPosts() {
-  fetch("http://localhost:3000/posts")
-    .then((res) => res.json())
-    .then((posts) => renderPosts(posts));
-}
+post.addEventListener("submit", Post.createPost);
 
-function renderPost(post) {
-  const postContainer = document.createElement("div");
-  postContainer.id = post.id;
-  postContainer.className = "post";
-
-  const userName = document.createElement("h3");
-  userName.innerHTML = post.user.name;
-
-  const content = document.createElement("p");
-  content.innerText = post.content;
-
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "Delete";
-  deleteButton.className = "delete";
-  deleteButton.addEventListener("click", () => deletePost(post));
-
-  // const timeStamp = document.createElement("div");
-  // timeStamp.innerText = post.created_at;
-
-  postContainer.append(userName, content, deleteButton);
-  posts.appendChild(postContainer);
-}
-
-function renderPosts(posts) {
-  for (let userPost of posts) {
-    renderPost(userPost);
-  }
-}
-
-post.addEventListener("submit", createPost);
-
-function createPost(e) {
-  e.preventDefault();
-  const content = document.querySelector(".twit").value;
-
-  fetch("http://localhost:3000/posts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ content: content }),
-  })
-    .then((res) => res.json())
-    .then((post) => renderPost(post));
-}
-
-function deletePost(post) {
-  fetch(`http://localhost:3000/posts/${post.id}`, {
-    method: "DELETE",
-  }).then((obj = document.getElementById(post.id)), obj.remove());
-}
-
-fetchPosts();
+Post.fetchPosts();
---
+++ twutter-frontend/src/post.js
@@ -0,0 +1,70 @@
+class Post {
+  constructor(post) {
+    (this.id = post.id), (this.content = post.content), (this.user = post.user);
+    Post.allPosts.push(this);
+  }
+
+  static allPosts = [];
+
+  static fetchPosts() {
+    fetch("http://localhost:3000/posts")
+      .then((res) => res.json())
+      .then((posts) => {
+        for (const post of posts) {
+          new Post(post);
+        }
+        this.renderPosts();
+      });
+  }
+
+  renderPost() {
+    const postContainer = document.createElement("div");
+    postContainer.id = this.id;
+    postContainer.className = "post";
+
+    const userName = document.createElement("h3");
+    userName.innerHTML = this.user.name;
+
+    const content = document.createElement("p");
+    content.innerText = this.content;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+    deleteButton.addEventListener("click", () => this.deletePost());
+
+    postContainer.append(userName, content, deleteButton);
+    posts.appendChild(postContainer);
+  }
+
+  static renderPosts() {
+    for (const userPost of this.allPosts) {
+      userPost.renderPost();
+    }
+  }
+
+  static createPost() {
+    event.preventDefault();
+    const content = document.querySelector(".twit").value;
+
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ content: content }),
+    })
+      .then((res) => res.json())
+      .then((post) => {
+        const newPost = new Post(post);
+        newPost.renderPost();
+      });
+  }
+
+  deletePost() {
+    fetch(`http://localhost:3000/posts/${this.id}`, {
+      method: "DELETE",
+    }).then(document.getElementById(this.id).remove());
+  }
+}
